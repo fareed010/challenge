fc6a61eaa6229547bd78ae13c5f85db54973fd77
40sherrin <fareedaskari42@gmail.com>
2022-09-08 23:14:01 +1000

    create character table

--- src/components/CharactersUI.jsx
+++ src/components/CharactersUI.jsx
@@ -6,8 +6,23 @@ import FilterSearch from "./FilterSearch";
 const CharacterUI = () => {                                             
     const {state} = useContext(CharactersContext)
 
-    const tableCharacter = state.data.map(i => {
-        return <p key={i.name}>{i.name}</p>
+    const tableCharacter = state.data.map((info, idx) => {
+        const {name, abilities, tags, thumbnail} = info;
+        
+        const abilityTD = abilities.map(ab => <td key={ab.abilityName} style={{color: ab.abilityScore === 10 ? 'red' : ''}}>{ab.abilityScore}</td>)
+        return (
+            <tbody>
+                <tr>
+                    <td>{info.name}</td>
+                    {
+                        tags && tags.length > 0 ? tags.map(i => <td key={i.tag_name}>{i.tag_name}</td>) : <td key="notag">NO TAG</td>
+                    }
+                    {
+                        abilityTD
+                    }            
+                </tr>
+            </tbody>
+        )
     })
 
     return (
@@ -16,9 +31,22 @@ const CharacterUI = () => {
 
             <FilterButton />
 
-            {
-                tableCharacter
-            }
+            <table>
+                <thead>
+                    <tr>
+                        <th>Character</th>
+                        <th>Tags</th>
+                        <th>Power</th>
+                        <th>Mobility</th>
+                        <th>Technique</th>
+                        <th>Survivability</th>
+                        <th>Energy</th>
+                    </tr>
+                </thead>
+                {
+                    tableCharacter
+                }
+            </table>
 
         </>
     )
